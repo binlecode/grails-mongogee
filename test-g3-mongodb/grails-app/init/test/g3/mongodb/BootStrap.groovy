59a9fe38f42e0026155dfc758546799be8cf9057
Bin Le <bin.le.code@gmail.com>
2017-08-01 22:20:57 -0400

    minor configuration enhancement for demo app deployment

--- test-g3-mongodb/grails-app/init/test/g3/mongodb/BootStrap.groovy
+++ test-g3-mongodb/grails-app/init/test/g3/mongodb/BootStrap.groovy
@@ -1,13 +1,22 @@
 package test.g3.mongodb
 
+import grails.core.GrailsApplication
 import grails.plugin.mongogee.MongogeeService
 
 class BootStrap {
-
+    GrailsApplication grailsApplication
     MongogeeService mongogeeService
 
-
     def init = { servletContext ->
+
+        log.info "mongodb url: ${grailsApplication.config.grails.mongodb.url}"
+
+        log.info "mongogee.changeLogsScanPackage: ${grailsApplication.config.grails.mongogee.changeLogsScanPackage}"
+        log.info "mongogee.changeEnabled: ${grailsApplication.config.mongogee.changeEnabled}"
+        log.info "mongogee.continueWithError: ${grailsApplication.config.mongogee.continueWithError}"
+        log.info "mongogee.lockingRetryEnabled: ${grailsApplication.config.mongogee.lockingRetryEnabled}"
+        log.info "mongogee.lockingRetryIntervalMillis: ${grailsApplication.config.mongogee.lockingRetryIntervalMillis}"
+        log.info "mongogee.lockingRetryMax: ${grailsApplication.config.mongogee.lockingRetryMax}"
 
         mongogeeService.execute()
 

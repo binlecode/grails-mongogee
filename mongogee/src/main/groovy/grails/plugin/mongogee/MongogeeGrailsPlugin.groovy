38ed198919e34e74ee3625434b839a8660d908f8
Bin Le <bin.le.code@gmail.com>
2018-01-04 10:40:31 -0500

    minor touch-up of some project info filling

--- mongogee/src/main/groovy/grails/plugin/mongogee/MongogeeGrailsPlugin.groovy
+++ mongogee/src/main/groovy/grails/plugin/mongogee/MongogeeGrailsPlugin.groovy
@@ -23,7 +23,7 @@ import groovy.util.logging.Slf4j
 class MongogeeGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.2.3 > *"
+    def grailsVersion = "3.2.0 > *"
     // make sure mongodb plugin is loaded first
     def loadAfter = ['mongodb']
     // resources that are excluded from plugin packaging
@@ -31,7 +31,6 @@ class MongogeeGrailsPlugin extends Plugin {
         "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
     def title = "Mongogee MongoDB data migration Grails plugin" // Headline display name of the plugin
     def author = "Bin Le"
     def authorEmail = "bin.le.code@gmail.com"
@@ -49,13 +48,13 @@ class MongogeeGrailsPlugin extends Plugin {
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def developers = [ [ name: "Kurt Bliefernich", email: "kblief@gmail.com" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "Github", url: "https://github.com/binlecode/mongogee/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/binlecode/mongogee" ]
 
     Closure doWithSpring() { {->
             // TODO Implement runtime spring config (optional)
